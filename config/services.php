b3083b1d1e675207f32af79fe5429968e081d474
Imen Frigui <imen_frigui@outlook.com>
2024-08-07 11:00:27 +0100

    import users command

--- config/services.php
+++ config/services.php
@@ -35,8 +35,8 @@ return [
         'client_id' => env('KEYCLOAK_CLIENT_ID'),
         'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
         'redirect' => env('KEYCLOAK_REDIRECT_URI'),
-        'base_url' => env('KEYCLOAK_BASE_URL'),   // Specify your keycloak server URL here
-        'realms' => env('KEYCLOAK_REALM')         // Specify your keycloak realm
+        'base_url' => env('KEYCLOAK_BASE_URL'),
+        'realms' => env('KEYCLOAK_REALM')
         ],
 
 
